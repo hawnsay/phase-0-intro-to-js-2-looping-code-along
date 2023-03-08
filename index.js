ef7afc9dd6de51223590728c70f32a2151aa4cf9
Shawn Chestine <shawn@Shawns-MBP.askey.com>
2023-03-07 21:30:20 -0800

    Completes looping lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solutions in this file
+const names = ["Guadalupe","Ollie","Aki"];
+let eventName = 'Event Name';
+
+function writeCards(names, value) {
+    let cardSent = []
+   for (let i = 0; i < names.length; i++) {
+        cardSent.push(`Thank you, ${names[i]}, for the wonderful ${value} gift!`);
+        console.log(cardSent)
+        debugger;
+   }
+
+    return cardSent;
+}
+
+writeCards(names, "surprise");
+
+function countDown(number) {
+   let x = number
+    while (x >= 0) {
+        console.log(x);
+        x--;
+    }
+return number;
+}
